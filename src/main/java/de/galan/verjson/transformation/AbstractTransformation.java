8648ae1de9055531206317ff98bdfb890f8abbf8
Daniel Galán y Martins <git@galan.de>
2014-02-26 08:49:27 +0100

    JavaDoc

--- src/main/java/de/galan/verjson/transformation/AbstractTransformation.java
+++ src/main/java/de/galan/verjson/transformation/AbstractTransformation.java
@@ -8,22 +8,25 @@ import com.google.gson.JsonObject;
 
 
 /**
- * daniel should have written a comment here.
+ * Provides common helpful methods fro transforming JsonElements.
  * 
  * @author daniel
  */
 public abstract class AbstractTransformation implements Transformation {
 
+	/** Returns the given element as JsonObject (calls getAsJsonObject()). */
 	protected JsonObject obj(JsonElement element) {
 		return element != null ? element.getAsJsonObject() : null;
 	}
 
 
+	/** Returns the field from a JsonObject as JsonObject */
 	protected JsonObject getObj(JsonObject obj, String fieldName) {
 		return obj == null ? null : obj(obj.get(fieldName));
 	}
 
 
+	/** Removes the field from a JsonObject and returns it as JsonObject */
 	protected JsonObject getObjAndRemove(JsonObject obj, String fieldName) {
 		JsonObject result = null;
 		if (obj != null) {
@@ -34,11 +37,16 @@ public abstract class AbstractTransformation implements Transformation {
 	}
 
 
+	/** Creates a JsonArray from the given elements, returns null if no elements are provided */
 	protected JsonArray createArray(JsonElement... elements) {
 		return createArray(false, elements);
 	}
 
 
+	/**
+	 * Creates a JsonArray from the given elements. Returns an empty JsonArray if no elements are provided and
+	 * fallbackToEmptyArray is true, null if false.
+	 */
 	protected JsonArray createArray(boolean fallbackToEmptyArray, JsonElement... elements) {
 		JsonArray array = null;
 		for (JsonElement element: elements) {
@@ -56,16 +64,19 @@ public abstract class AbstractTransformation implements Transformation {
 	}
 
 
+	/** Returns the given element as JsonArray (calls getAsJsonArray()). */
 	protected JsonArray array(JsonElement element) {
 		return element != null ? element.getAsJsonArray() : null;
 	}
 
 
+	/** Returns the field from a JsonObject as JsonArray */
 	protected JsonArray getArray(JsonObject obj, String fieldName) {
 		return obj == null ? null : array(obj.get(fieldName));
 	}
 
 
+	/** Removes the field from a JsonObject and returns it as JsonArray */
 	protected JsonArray getArrayAndRemove(JsonObject obj, String fieldName) {
 		JsonArray result = null;
 		if (obj != null) {
@@ -76,6 +87,7 @@ public abstract class AbstractTransformation implements Transformation {
 	}
 
 
+	/** Removes the field with the given name from the given JsonObject */
 	protected void remove(JsonObject obj, String fieldNameToRemove) {
 		if (obj != null) {
 			obj.remove(fieldNameToRemove);
@@ -83,6 +95,7 @@ public abstract class AbstractTransformation implements Transformation {
 	}
 
 
+	/** Renames a field in a JsonObject from the oldFieldName to the newFieldName */
 	protected void rename(JsonObject obj, String oldFieldName, String newFieldName) {
 		if (obj != null && isNotBlank(oldFieldName) && isNotBlank(newFieldName)) {
 			JsonElement elem = obj.get(oldFieldName);
