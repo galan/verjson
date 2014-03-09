e7b3a0396665a8812ac007e2f177030af03bfa4b
Daniel Galán y Martins <git@galan.de>
2014-03-09 15:08:14 +0100

    test

---
+++ src/test/java/de/galan/verjson/jackson/PolyTest.java
@@ -0,0 +1,20 @@
+package de.galan.verjson.jackson;
+
+import org.junit.Test;
+
+import de.galan.commons.test.AbstractTestParent;
+
+
+/**
+ * daniel should have written a comment here.
+ * 
+ * @author daniel
+ */
+public class PolyTest extends AbstractTestParent {
+
+	@Test
+	public void testName() throws Exception {
+
+	}
+
+}
