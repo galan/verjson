9bedcde9c4e5c240e3868eff43acec0028e8e671
Daniel Galán y Martins <git@galan.de>
2014-03-10 21:12:32 +0100

    test jackson

--- src/test/java/de/galan/verjson/jackson/PolyTest.java
+++ src/test/java/de/galan/verjson/jackson/PolyTest.java
@@ -1,20 +1,31 @@
 package de.galan.verjson.jackson;
 
 import org.junit.Test;
+import org.slf4j.Logger;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.galan.commons.logging.Logr;
 import de.galan.commons.test.AbstractTestParent;
+import de.galan.verjson.example.v1.Example1;
 
 
 /**
- * daniel should have written a comment here.
+ * Tests for jackson
  * 
  * @author daniel
  */
 public class PolyTest extends AbstractTestParent {
 
+	private final static Logger LOG = Logr.get();
+
+
 	@Test
 	public void testName() throws Exception {
-
+		Example1 e1 = Example1.createSample();
+		ObjectMapper mapper = new ObjectMapper();
+		String output = mapper.writeValueAsString(e1);
+		LOG.info(output);
 	}
 
 }
