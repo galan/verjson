ba2189421c8fd39df22ae4a41d9559b797023bc3
Daniel Galán y Martins <git@galan.de>
2014-04-15 10:47:43 +0200

    json schema validation

---
+++ src/main/java/de/galan/verjson/step/validation/InvalidJsonException.java
@@ -0,0 +1,19 @@
+package de.galan.verjson.step.validation;
+
+/**
+ * JSON does not validate agains JSON Schema.
+ * 
+ * @author daniel
+ */
+public class InvalidJsonException extends RuntimeException {
+
+	public InvalidJsonException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+
+	public InvalidJsonException(String message) {
+		super(message);
+	}
+
+}
--- src/main/java/de/galan/verjson/step/validation/Validation.java
+++ src/main/java/de/galan/verjson/step/validation/Validation.java
@@ -1,6 +1,13 @@
 package de.galan.verjson.step.validation;
 
+import java.io.IOException;
+
 import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jackson.JsonLoader;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
 
 import de.galan.verjson.step.Step;
 
@@ -12,14 +19,72 @@ import de.galan.verjson.step.Step;
  */
 public class Validation implements Step {
 
+	JsonSchema schema; // thread-safe
+	static JsonSchemaFactory factory; // cached
+
+
 	public Validation(String schema) {
-		// ..
+		this(schema, null);
+	}
+
+
+	public Validation(String schema, String description) {
+		this.schema = create(schema, description);
+	}
+
+
+	protected static synchronized JsonSchemaFactory getFactory() {
+		if (factory == null) {
+			factory = JsonSchemaFactory.byDefault();
+		}
+		return factory;
+	}
+
+
+	protected JsonSchemaFactory getJsonSchemaFactory() {
+		return getFactory();
 	}
 
 
 	@Override
 	public void process(JsonNode node) {
-		// perform validation
+		validate(node);
+	}
+
+
+	protected JsonSchema getSchema() {
+		return schema;
+	}
+
+
+	public void validate(JsonNode node) {
+		try {
+			ProcessingReport report = getSchema().validate(node);
+			//for (ProcessingMessage message: report) {
+			//}
+			if (!report.isSuccess()) {
+				throw new InvalidJsonException(report.toString());
+			}
+		}
+		catch (ProcessingException ex) {
+			throw new InvalidJsonException("Could not validate JSON against schema", ex);
+		}
+	}
+
+
+	public JsonSchema create(String schemaString, String description) {
+		JsonSchema jsonSchema = null;
+		try {
+			JsonNode schemaNode = JsonLoader.fromString(schemaString);
+			if (!getJsonSchemaFactory().getSyntaxValidator().schemaIsValid(schemaNode)) {
+				throw new InvalidSchemaException("JSON Schema is invalid (" + description + ")");
+			}
+			jsonSchema = getJsonSchemaFactory().getJsonSchema(schemaNode);
+		}
+		catch (IOException | ProcessingException ex) {
+			throw new InvalidSchemaException("JSON Schema could not be loaded (" + description + ")", ex);
+		}
+		return jsonSchema;
 	}
 
 }
