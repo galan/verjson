c3081615fc8c388f016670a0835a8293891e71a0
Daniel Galán y Martins <git@galan.de>
2014-07-01 09:05:37 +0200

    preparing test

---
+++ src/test/java/de/galan/verjson/examples/polymorphism/PolymorphismTest.java
@@ -0,0 +1,20 @@
+package de.galan.verjson.examples.polymorphism;
+
+import org.junit.Test;
+
+import de.galan.commons.test.AbstractTestParent;
+
+
+/**
+ * Demonstrates working with polymorh types.
+ *
+ * @author daniel
+ */
+public class PolymorphismTest extends AbstractTestParent {
+
+	@Test
+	public void testName() throws Exception {
+		//TODO
+	}
+
+}
